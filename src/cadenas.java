3fb1e1eb348d068ebc54debc7a83bf18eea9c7a8
Luis Castillo <apointer252@gmail.com>
2024-09-28 18:04:58 -0600

    Added print with text block

--- src/cadenas.java
+++ src/cadenas.java
@@ -11,5 +11,27 @@ public class cadenas {
 
 				//Method printf
 				System.out.printf("Name: %s, Age: %d, Salary: $%.2f%n", name, age, salary);
+
+				// Format with text block
+				var employeeID = 12;
+				message = """
+                %nEmployee Details:\s
+                -----------------
+                \tName: %s
+                \tID: %04d
+                \tAge: %d años
+                \tSalary: $%.2f
+                """.formatted(name, employeeID, age, salary);
+				System.out.println(message);
+
+				// Format with tex block y printf directly
+				System.out.printf("""
+                %nEmployee Details:\s
+                -----------------
+                \tName: %s
+                \tID: %04d
+                \tAge: %d años
+                \tSalary: $%.2f
+                """, name, employeeID, age, salary);
 		}
 }
